cd62d7ed519bb02fe79a3b68f0243fb7e01389f9
Morgan Denney <morgdenney@gmail.com>
2015-03-09 19:46:39 -0400

    got ratche working, added some styling to jots/index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Vendored Fonts
+# http://stackoverflow.com/questions/10905905/using-fonts-with-rails-asset-pipeline
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :jots
+  resources :home 
 
 end
