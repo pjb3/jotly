9840d11f5f578f645bb26a2d0795a27f767b1355
Morgan Denney <morgdenney@gmail.com>
2015-03-09 19:24:57 -0400

    add vendor files

--- app/controllers/jots_controller.rb
+++ app/controllers/jots_controller.rb
@@ -14,6 +14,16 @@ class JotsController < ApplicationController
     @jots = Jot.all
   end
 
+  def edit
+    @jot = Jot.find(params[:id])
+  end
+
+  def update
+    jot = Jot.find(params[:id])
+    jot.update(jot_params)
+
+    redirect_to jots_path
+  end
 
   def jot_params
     params.require(:jot).permit(:message)
