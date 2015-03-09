47b0888365dbf200552a9e50362936469e56b602
Morgan Denney <morgdenney@gmail.com>
2015-03-09 19:06:59 -0400

    make index work display all jots

--- app/controllers/jots_controller.rb
+++ app/controllers/jots_controller.rb
@@ -1,6 +1,23 @@
-class JotController < ApplicationController
+class JotsController < ApplicationController
 
   def new
     @jot = Jot.new
   end
+
+  def create
+    @jot = Jot.create(jot_params)
+
+    redirect_to jots_path
+  end
+
+  def index
+    @jots = Jot.all
+  end
+
+
+  def jot_params
+    params.require(:jot).permit(:message)
+  end
+
+
 end
