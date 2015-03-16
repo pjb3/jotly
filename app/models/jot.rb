08ac62a840fe4ad20ca8a7d6287d136676c385ea
Morgan Denney <morgdenney@gmail.com>
2015-03-16 18:08:59 -0400

    resized photos for thumbs and in show

--- app/models/jot.rb
+++ app/models/jot.rb
@@ -3,7 +3,8 @@ class Jot < ActiveRecord::Base
   has_many :likes
   # , counter_cache :true
   has_attached_file :photo,
-    styles: { medium: "100x100>" },
+    styles: { medium: "300x300>",
+      thumb: "100x100"},
     default_url: "http://placehold.it/100x100"
   validates_attachment_content_type :photo,
     content_type: /\Aimage\/.*\Z/
